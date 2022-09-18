73059fe51b41027fdf0b4b9ee3a1f7d76ec0f575
ahsidd96 <ahsidd96@gmail.com>
2022-09-18 21:51:54 +0500

    https modifications removed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
-const https = require("https");
 const http = require("http");
-const fs = require("fs");
 
 function onRequest(req, res) {
   console.log("serve: " + req.url);
@@ -34,40 +32,6 @@ function onRequest(req, res) {
   });
 }
 
-function onRequestHTTPS(req, res) {
-  console.log("serve: " + req.url);
-
-  if (!req.headers.host) {
-    res.writeHead(400, { "Content-Type": "text/html" });
-    res.write("Invalid header");
-    res.end();
-  }
-
-  const options = {
-    hostname: req.headers.host.split(":")[0],
-    port: req.headers.host.split(":")[1] ?? 3000,
-    path: req.url,
-    method: req.method,
-    headers: {
-      ...req.headers,
-    },
-    timeout: 180000,
-  };
-
-  const proxy = https.request(options, function (r) {
-    res.writeHead(r.statusCode, r.headers);
-    r.pipe(res, {
-      end: true,
-    });
-  });
-
-  req.pipe(proxy, {
-    end: true,
-  });
-}
-
 http.createServer(onRequest).listen(3003);
 
-// Create an HTTPS service identical to the HTTP service.
-https.createServer(onRequestHTTPS).listen(443);
 console.log("Listening on port 3003");
